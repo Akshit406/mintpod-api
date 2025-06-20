b742af312334bb40ae4c31786a9e776cb34a4673
Akshit406 <akshitsaxena450@gmail.com>
2025-06-20 11:04:10 +0530

    db config

---
+++ config/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+ const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log("mongoDB connected.")
+    }
+    catch(error) {
+        console.error("Error connecting mongoDB: ", error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
